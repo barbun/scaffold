d16279d33baa033f7a473e13105fe16f9dd56bf3
barbun <robarcover@gmail.com>
2026-04-23 12:04:33 +1000

    Added HTTPAV identifier.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -8,7 +8,13 @@ ARG GOVCMS_IMAGE_VERSION={{ GOVCMS_VERSION }}.x-latest
 FROM ${CLI_IMAGE} as cli
 FROM govcms/php:${GOVCMS_IMAGE_VERSION}
 
+ARG LAGOON_PROJECT
+ARG LAGOON_ENVIRONMENT
+
 # Clean up base image so as not to conflict with any changes.
 RUN rm -rf /app
 
 COPY --from=cli /app /app
+
+# Define HTTPAV identifier.
+ENV HTTPAV_IDENTIFIER=${LAGOON_PROJECT}-${LAGOON_ENVIRONMENT}
